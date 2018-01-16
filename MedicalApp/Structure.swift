fc31c3a3d1746430ea246139ac52b496bfc06f40
seenashafai <seenas@btinternet.com>
2018-01-16 13:10:41 +0000

    Created Views

---
+++ MedicalApp/Structure.swift
@@ -0,0 +1,18 @@
+//
+//  Structure.swift
+//  MedicalApp
+//
+//  Created by Seena Shafai on 15/01/2018.
+//  Copyright © 2018 Seena Shafai. All rights reserved.
+//
+
+import Foundation
+
+struct patient {
+    var firstName: String
+    var surname: String
+    var patientID: Int
+    var meds = [[String]]()
+}
+
+
